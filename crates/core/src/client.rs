6faf518e12fc9f73b0cec20c268c41b64284b299
Andrew Kozin <andrew.kozin@gmail.com>
2024-05-19 10:32:46 +0100

    Add Client trait and its PostgresClient implementation

---
+++ crates/core/src/client.rs
@@ -0,0 +1,129 @@
+use std::error::Error as StdError;
+use std::fmt::{Display, Formatter, Result as FmtResult};
+
+#[cfg(feature = "postgres")]
+use postgres::{Error as PostgresError, Row as PostgresRow};
+#[cfg(feature = "postgres")]
+#[allow(unused_imports)]
+use postgres_from_row::FromRow;
+
+#[allow(dead_code)]
+pub trait TryFromRow<Row>
+where
+    Self: Sized,
+{
+    fn try_from_row(row: &Row) -> Result<Self, ParseRowError>;
+}
+
+#[cfg(feature = "postgres")]
+impl<T: FromRow> TryFromRow<PostgresRow> for T {
+    fn try_from_row(row: &PostgresRow) -> Result<Self, ParseRowError> {
+        <T as FromRow>::try_from_row(row).map_err(ParseRowError::Postgres)
+    }
+}
+
+/// Interface to interact with a database
+#[allow(dead_code)]
+pub trait Client {
+    type Row;
+    fn query(&mut self, query: &str) -> Result<Vec<Self::Row>, ExecuteQueryError>;
+}
+
+#[derive(Debug)]
+pub enum EstablishConnectionError {
+    #[cfg(feature = "postgres")]
+    Postgres(PostgresError),
+}
+
+impl Display for EstablishConnectionError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        match self {
+            #[cfg(feature = "postgres")]
+            Self::Postgres(err) => write!(f, "{}", err),
+        }
+    }
+}
+
+impl StdError for EstablishConnectionError {
+    fn source(&self) -> Option<&(dyn StdError + 'static)> {
+        match self {
+            #[cfg(feature = "postgres")]
+            Self::Postgres(err) => Some(err),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum ExecuteQueryError {
+    #[cfg(feature = "postgres")]
+    Postgres(PostgresError),
+}
+
+impl Display for ExecuteQueryError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        match self {
+            #[cfg(feature = "postgres")]
+            Self::Postgres(err) => write!(f, "{}", err),
+        }
+    }
+}
+
+impl StdError for ExecuteQueryError {
+    fn source(&self) -> Option<&(dyn StdError + 'static)> {
+        match self {
+            #[cfg(feature = "postgres")]
+            Self::Postgres(err) => Some(err),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum ParseRowError {
+    #[cfg(feature = "postgres")]
+    Postgres(PostgresError),
+}
+
+impl Display for ParseRowError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        match self {
+            #[cfg(feature = "postgres")]
+            Self::Postgres(err) => write!(f, "{}", err),
+        }
+    }
+}
+
+impl StdError for ParseRowError {
+    fn source(&self) -> Option<&(dyn StdError + 'static)> {
+        match self {
+            #[cfg(feature = "postgres")]
+            Self::Postgres(err) => Some(err),
+        }
+    }
+}
+
+#[cfg(feature = "postgres")]
+#[repr(C)]
+pub struct PostgresClient {
+    conn: postgres::Client,
+}
+
+#[cfg(feature = "postgres")]
+#[allow(dead_code)]
+impl PostgresClient {
+    pub fn connect(url: &str) -> Result<Self, EstablishConnectionError> {
+        postgres::Client::connect(url, postgres::NoTls)
+            .map_err(EstablishConnectionError::Postgres)
+            .map(|conn| Self { conn })
+    }
+}
+
+#[cfg(feature = "postgres")]
+impl Client for PostgresClient {
+    type Row = PostgresRow;
+
+    fn query(&mut self, query: &str) -> Result<Vec<Self::Row>, ExecuteQueryError> {
+        self.conn
+            .query(query, &[])
+            .map_err(ExecuteQueryError::Postgres)
+    }
+}
--- crates/core/src/error.rs
+++ crates/core/src/error.rs
@@ -2,18 +2,25 @@ use std::error::Error as StdError;
 use std::fmt::{Display, Formatter, Result as FmtResult};
 use std::result::Result as StdResult;
 
+use crate::client::{EstablishConnectionError, ExecuteQueryError, ParseRowError};
 use crate::to_sql::Error as ToSqlError;
 
 pub type Result<T> = StdResult<T, Error>;
 
 #[derive(Debug)]
 pub enum Error {
+    EstablishConnection(EstablishConnectionError),
+    ExecuteQuery(ExecuteQueryError),
+    ParseRow(ParseRowError),
     RenderSql(ToSqlError),
 }
 
 impl Display for Error {
     fn fmt(&self, f: &mut Formatter) -> FmtResult {
         match self {
+            Self::EstablishConnection(err) => write!(f, "Failed to establish connection: {}", err),
+            Self::ExecuteQuery(err) => write!(f, "Failed to execute query: {}", err),
+            Self::ParseRow(err) => write!(f, "Failed to parse row: {}", err),
             Self::RenderSql(err) => write!(f, "Failed to render SQL WHERE clause: {}", err),
         }
     }
@@ -22,6 +29,9 @@ impl Display for Error {
 impl StdError for Error {
     fn source(&self) -> Option<&(dyn StdError + 'static)> {
         match self {
+            Self::EstablishConnection(err) => Some(err),
+            Self::ExecuteQuery(err) => Some(err),
+            Self::ParseRow(err) => Some(err),
             Self::RenderSql(err) => Some(err),
         }
     }
@@ -30,5 +40,23 @@ impl StdError for Error {
 impl From<ToSqlError> for Error {
     fn from(err: ToSqlError) -> Self {
         Self::RenderSql(err)
+    }
+}
+
+impl From<EstablishConnectionError> for Error {
+    fn from(err: EstablishConnectionError) -> Self {
+        Self::EstablishConnection(err)
+    }
+}
+
+impl From<ExecuteQueryError> for Error {
+    fn from(err: ExecuteQueryError) -> Self {
+        Self::ExecuteQuery(err)
+    }
+}
+
+impl From<ParseRowError> for Error {
+    fn from(err: ParseRowError) -> Self {
+        Self::ParseRow(err)
     }
 }
